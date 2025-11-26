f0720d68624bac4e3251c51f58a6651e67611c57
Marco Widesott <marco.widesott@anitia.it>
2025-11-26 15:03:00 +0100

    init os && flow

--- main.go
+++ main.go
@@ -9,7 +9,19 @@ func main() {
 	//os.Args legge gli argomenti all' interno dell' arg che gli vengono passati, un
 	//args[0] sarà il nome del programma
 	args := os.Args
-	fmt.Println("logcli inizializzato!")
-	fmt.Println("Numero di argomenti: ", len(args))
-	fmt.Println("Contenuto args: ", args)
+
+	//Almeno due elementi in args
+
+	if len(args) < 2 {
+		fmt.Println("Inserisci più di due file.")
+		os.Exit(1) //Errore utente
+	}
+
+	//Inserisco il nome del programma e gli argomenti che verranno passati successivamente
+	programName := args[0]
+	filePaths := args[1:]
+
+	//Stampo a console il nome del programma e i file passati in input
+	fmt.Println("Programma: ", programName)
+	fmt.Println("Files: ", filePaths)
 }
