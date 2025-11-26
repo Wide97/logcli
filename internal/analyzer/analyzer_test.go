8e546f63a56243e62ee1889538fbb18ed1d801e5
Marco Widesott <marco.widesott@anitia.it>
2025-11-26 17:49:37 +0100

    added first test

---
+++ internal/analyzer/analyzer_test.go
@@ -0,0 +1,30 @@
+package analyzer
+
+import "testing"
+
+func TestClassifyLine(t *testing.T) {
+	//Imposto la struttura
+	test := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		//Fornisco dei casi di test
+		{"info uppercase", "INFO Starting application", "info"},
+		{"info lowercase", "info: something", "info"},
+		{"warn uppercase", "WARN Disk almost full", "warn"},
+		{"warn mixed", "WaRn something", "warn"},
+		{"error uppercase", "ERROR Fatal exception", "error"},
+		{"error lowercase", "error occurred", "error"},
+		{"no keyword", "this line has no level", "other"},
+	}
+	//Verifico il funzionamento chiamando il metodo classifyline
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ClassifyLine(tt.input)
+			if got != tt.expected {
+				t.Fatalf("ClassifyLine(%q) = %q, expected %q", tt.input, got, tt.expected)
+			}
+		})
+	}
+}
